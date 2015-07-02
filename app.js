848a34f51a45320531abc10845d69909643e9804
David Deuber <ddeuber@fastmail.fm>
2015-07-02 15:30:33 -0700

    hw

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var app = angular.module('pokemonCrudApp', ['ngRoute']);
 
-// app.controller('MainCtrl', function($scope){
-//   $scope.hello = "TESTING..."
-// })
+app.config(function($routeProvider){
 
+  $routeProvider
+    .when('/', {
+      template: '{{ hello }} this works!',
+      controller: 'MainCtrl'
+    })
+
+})
+
+app.controller('MainCtrl', function($scope){
+  $scope.hello = "TESTING..."
+})
